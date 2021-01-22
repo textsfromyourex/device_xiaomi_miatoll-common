c713c44d543811b83d26f01f4c0d5df243a6733e
SonalSingh18 <sonal.singh.19993@gmail.com>
2021-01-22 18:22:28 +0530

    sm6250-common: titanium -> havoc
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- common.mk
+++ common.mk
@@ -279,7 +279,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-titanium
+    $(LOCAL_PATH)/overlay-havoc
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-proton
+git clone https://github.com/LineageOS/android_vendor_qcom_opensource_healthd-ext.git -b lineage-18.1 vendor/qcom/opensource/healthd-ext
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-18.1 hardware/xiaomi
